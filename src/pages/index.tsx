dd19575633fef2ac283c4e3a291a469e11cb4c47
Ben Elferink <ben.elferink@icloud.com>
2024-03-12 18:08:39 +0200

    pos: absolute -> fixed

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -39,7 +39,7 @@ export default function Page({ partnerName, partnerCode }: DexHunterProps) {
           backgroundSize: 'cover',
           backgroundPosition: 'center',
 
-          position: isMobile ? 'relative' : 'absolute',
+          position: isMobile ? 'relative' : 'fixed',
           top: '50%',
           right: isMobile ? 0 : 350 * scale,
           left: 'unset',
@@ -78,11 +78,11 @@ export default function Page({ partnerName, partnerCode }: DexHunterProps) {
 
       {isMobile ? (
         <Fragment>
-          <div className='absolute bottom-10 right-40 animate-[point_0.5s_alternate_infinite]'>
+          <div className='fixed bottom-10 right-40 animate-[point_0.5s_alternate_infinite]'>
             <Image src='/media/transparent/arm.png' alt='arm' width={300 * 0.65} height={208 * 0.65} priority unoptimized />
           </div>
 
-          <div className='absolute bottom-0 right-0'>
+          <div className='fixed bottom-0 right-0'>
             <Image src='/media/transparent/sharl.png' alt='sharl' width={459 * 0.65} height={492 * 0.65} priority unoptimized />
           </div>
         </Fragment>
@@ -92,16 +92,16 @@ export default function Page({ partnerName, partnerCode }: DexHunterProps) {
             href='https://x.com/IOHK_Charles/status/1765109763388649609'
             target='_blank'
             rel='noopener noreferrer'
-            className='hidden sm:block absolute top-20 right-6 cursor-pointer hover:scale-125 hover:rotate-12 duration-500'
+            className='hidden sm:block fixed top-20 right-6 cursor-pointer hover:scale-125 hover:rotate-12 duration-500'
           >
             <Image src='/media/wall_art.png' alt='wall_art' width={1362 * 0.2 * scale} height={1580 * 0.2 * scale} priority unoptimized />
           </Link>
 
-          <div className='absolute bottom-10 right-60 animate-[point_0.5s_alternate_infinite]'>
+          <div className='fixed bottom-10 right-60 animate-[point_0.5s_alternate_infinite]'>
             <Image src='/media/transparent/arm.png' alt='arm' width={300 * 1 * scale} height={208 * 1 * scale} priority unoptimized />
           </div>
 
-          <div className='absolute bottom-0 right-0'>
+          <div className='fixed bottom-0 right-0'>
             <Image src='/media/transparent/sharl.png' alt='sharl' width={459 * 1 * scale} height={492 * 1 * scale} priority unoptimized />
           </div>
         </Fragment>
